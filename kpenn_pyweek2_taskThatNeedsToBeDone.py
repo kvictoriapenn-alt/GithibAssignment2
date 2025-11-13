b88369f3c1be5870d1b0bcc2d8fae97770ae3e72
kvictoriapenn-alt <kvictoriapenn@gmail.com>
2025-11-13 18:37:27 -0500

    Add files via upload

---
+++ kpenn_pyweek2_taskThatNeedsToBeDone.py
@@ -0,0 +1,9 @@
+task = input("What is the task that needs to be done?")
+
+importance = input("How important is this task? on a scale 1-5")
+
+deadline = input ("How much time do you have to do this?")
+
+if difficulty == 1
+    print ("That sounds pretty easy.")
+
